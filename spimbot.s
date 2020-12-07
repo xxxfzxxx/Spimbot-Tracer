88dcdd3c1197ff3bd47f774116290c4305b8c012
Eric Zhou <yiruiz2@illinois.edu>
2020-12-07 04:13:46 -0600

    lab_spimbot done

--- spimbot.s
+++ spimbot.s
@@ -70,7 +70,9 @@ silo_position_on_map:   .word 0
 select_region:          .word 0x06120814
 minibot_info:           .word 0
                         .word 0:12
-bot_estimate_move_time: .word 0:6
+minibots_estimate_arriving_time:     
+                        .word 0:6
+minibots_target_tile:   .byte -1:12
 current_checking_idx:   .word 0
 ### Minibot control
 
@@ -126,83 +128,43 @@ original_map:           .byte 0:1600
 floor_indices:          .word 0:3200 
 kernel_locations:       .word 0
                         .byte 0:1600
-left_map_coordinate:    .byte 0  0  1  0  2  0  3  0  4  0
-                        .byte 0  1  1  1  2  1  3  1  4  1
-                        .byte 0  2  1  2  2  2  3  2  4  2
-                        .byte 0  3  1  3  2  3  3  3  4  3  5  3
+left_map_coordinate:    .byte 5  19 6  19 7  19 8  19 9  19 14 19 15 19 16 19 17 19 18 19 19 19 20 19 21 19 22 19 23 19 24 19
+                        .byte 5  20 6  20 7  20 8  20 14 20 15 20 16 20 17 20 18 20 19 20 20 20 21 20 22 20 23 20 24 20
+                        .byte 5  18 6  18 7  18 8  18 9  18 10 18 14 18 15 18 16 18 17 18 18 18 19 18 20 18 21 18 22 18 23 18 24 18
+                        .byte 4  21 5  21 6  21 7  21 8  21 14 21 15 21 16 21 17 21 18 21 19 21 20 21 21 21 22 21 23 21 24 21
+                        .byte 5  17 6  17 7  17 8  17 9  17 10 17 11 17 14 17 15 17 16 17 17 17 18 17 19 17 20 17 21 17 22 17 23 17 24 17
+                        .byte 3  22 4  22 5  22 6  22 7  22 8  22 9  22 14 22 15 22 16 22 17 22 18 22 19 22 20 22 21 22 22 22 23 22 24 22
+                        .byte 5  16 6  16 7  16 9  16 10 16 11 16 12 16 14 16 15 16 16 16 17 16 18 16 19 16 20 16 21 16 22 16 23 16 24 16
+                        .byte 3  23 4  23 5  23 6  23 7  23 8  23 9  23 14 23 15 23 16 23 17 23 18 23 19 23 20 23 21 23 22 23 23 23 24 23
+                        .byte 5  15 6  15 7  15 10 15 11 15 12 15 13 15 14 15 15 15 16 15 17 15 18 15 19 15 20 15 21 15 22 15 23 15 24 15
+                        .byte 2  24 3  24 4  24 5  24 6  24 7  24 8  24 9  24 10 24 14 24 15 24 16 24 17 24 18 24 19 24 20 24 21 24 22 24 23 24 24 24
+                        .byte 5  14 6  14 7  14 11 14 12 14 13 14 14 14 15 14 16 14 17 14 18 14 19 14 20 14 21 14 22 14 23 14 24 14
+                        .byte 2  25 3  25 4  25 5  25 6  25 7  25 8  25 9  25 10 25 14 25 15 25 16 25 17 25 18 25 19 25 20 25 21 25 22 25 23 25 24 25
+                        .byte 5  13 6  13 7  13 11 13 12 13 13 13 14 13 15 13 16 13 17 13 18 13 19 13 20 13 21 13 22 13 23 13 24 13
+                        .byte 2  26 3  26 4  26 5  26 6  26 7  26 8  26 9  26 10 26 11 26 12 26 13 26 14 26 15 26 16 26 17 26 18 26 19 26 20 26 21 26 22 26 23 26 24 26
+                        .byte 5  12 6  12 7  12 10 12 11 12 12 12 13 12 14 12 15 12 16 12 17 12 18 12 19 12 20 12 21 12 22 12 23 12 24 12
+                        .byte 2  27 3  27 4  27 5  27 6  27 7  27 8  27 9  27 10 27 11 27 12 27 13 27 14 27 15 27 16 27 17 27 18 27 19 27 20 27 21 27 22 27 23 27 24 27
+                        .byte 5  11 6  11 7  11 9  11 10 11 11 11 12 11 13 11 15 11 16 11 17 11 18 11 19 11 20 11 21 11 22 11 23 11 24 11
+                        .byte 2  28 3  28 4  28 5  28 6  28 7  28 8  28 9  28 10 28 11 28 12 28 13 28 14 28 15 28 16 28 17 28 18 28 19 28 20 28 21 28 22 28 23 28 24 28
+                        .byte 5  10 6  10 7  10 8  10 9  10 10 10 11 10 12 10 15 10 16 10 17 10 18 10 19 10 20 10 21 10 22 10 23 10 24 10
+                        .byte 2  29 3  29 4  29 5  29 6  29 7  29 8  29 9  29 10 29 11 29 12 29 13 29 14 29 15 29 16 29 17 29 18 29 19 29 20 29 21 29 22 29 23 29 24 29
+                        .byte 5  9  6  9  7  9  8  9  9  9  10 9  11 9  16 9  17 9  18 9  19 9  20 9  21 9  22 9  23 9
+                        .byte 2  30 3  30 4  30 5  30 6  30 7  30 8  30 9  30 10 30 16 30 17 30 18 30 19 30 20 30 21 30 22 30 23 30
+                        .byte 5  8  6  8  7  8  8  8  9  8  10 8  16 8  17 8  18 8  19 8  20 8  21 8  22 8  23 8
+                        .byte 2  31 3  31 4  31 5  31 6  31 7  31 8  31 9  31 10 31 16 31 17 31 18 31 19 31 20 31 21 31 22 31 23 31
+                        .byte 5  7  6  7  7  7  8  7  9  7  17 7  18 7  19 7  20 7  21 7  22 7
+                        .byte 3  32 4  32 5  32 6  32 7  32 8  32 9  32 17 32 18 32 19 32 20 32 21 32 22 32
+                        .byte 4  6  5  6  6  6  7  6  8  6
+                        .byte 3  33 4  33 5  33 6  33 7  33 8  33 9  33
+                        .byte 3  5  4  5  5  5  6  5  7  5
+                        .byte 4  34 5  34 6  34 7  34 8  34
                         .byte 0  4  1  4  2  4  3  4  4  4  5  4  6  4
-                        .byte 3  5  4  5  5  5  6  5  7  5  8  5  9  5  10 5  11 5  12 5  13 5
-                        .byte 4  6  5  6  6  6  7  6  8  6  9  6  10 6  11 6  12 6  13 6
-                        .byte 5  7  6  7  7  7  8  7  9  7  10 7  11 7  12 7  13 7
-                        .byte 6  8  7  8  8  8  9  8  10 8
-                        .byte 7  9  8  9  9  9  10 9  11 9
-                        .byte 8  10 9  10 10 10 11 10 12 10
-                        .byte 9  11 10 11 11 11 12 11 13 11
-                        .byte 10 12 11 12 12 12 13 12
-                        .byte 11 13 12 13 13 13
-                        .byte 12 14 13 14
-                        .byte 10 15 11 15 12 15 13 15
-                        .byte 8  16 9  16 10 16 11 16 12 16 13 16
-                        .byte 7  17 8  17 9  17 10 17 11 17 12 17 13 17
-                        .byte 7  18 8  18 9  18 10 18 11 18 12 18 13 18
-                        .byte 7  19 8  19 9  19 10 19 11 19 12 19 13 19
-                        .byte 7  20 8  20 9  20 10 20 11 20 12 20 13 20
-                        .byte 7  21 8  21 9  21 10 21 11 21 12 21 13 21
-                        .byte 7  22 8  22 9  22 10 22 11 22 12 22 13 22
-                        .byte 8  23 9  23 10 23 11 23 12 23 13 23
-                        .byte 10 24 11 24 12 24 13 24
-                        .byte 10 25 11 25 12 25 13 25
-                        .byte 10 26 11 26 12 26 13 26
-                        .byte 10 27 11 27 12 27 13 27
-                        .byte 10 28 11 28 12 28 13 28
-                        .byte 8  29 9  29 10 29 11 29 12 29 13 29
-                        .byte 6  30 7  30 8  30 9  30 10 30 11 30 12 30 13 30
-                        .byte 5  31 6  31 7  31 8  31 9  31 10 31 11 31 12 31 13 31
-                        .byte 5  32 6  32 7  32 8  32 9  32 10 32 11 32 12 32 13 32
-                        .byte 5  33 6  33 7  33 8  33 9  33 10 33 11 33 12 33 13 33
-                        .byte 5  34 6  34 7  34 8  34 9  34 10 34 11 34 12 34 13 34
-                        .byte 5  35 6  35 7  35 8  35 9  35 10 35 11 35 12 35 13 35
-                        .byte 6  36 7  36 8  36 9  36 10 36 11 36 12 36 13 36
-                        .byte 8  37 9  37 10 37 11 37 12 37 13 37
-                        .byte -1
-# (0 ,0 ),(1 ,0 ),(2 ,0 ),(3 ,0 ),(4 ,0 ),
-# (0 ,1 ),(1 ,1 ),(2 ,1 ),(3 ,1 ),(4 ,1 ),
-# (0 ,2 ),(1 ,2 ),(2 ,2 ),(3 ,2 ),(4 ,2 ),
-# (0 ,3 ),(1 ,3 ),(2 ,3 ),(3 ,3 ),(4 ,3 ),(5 ,3 ),
-# (0 ,4 ),(1 ,4 ),(2 ,4 ),(3 ,4 ),(4 ,4 ),(5 ,4 ),(6 ,4 ),
-# (3 ,5 ),(4 ,5 ),(5 ,5 ),(6 ,5 ),(7 ,5 ),(8 ,5 ),(9 ,5 ),(10,5 ),(11,5 ),(12,5 ),(13,5 ),
-# (4 ,6 ),(5 ,6 ),(6 ,6 ),(7 ,6 ),(8 ,6 ),(9 ,6 ),(10,6 ),(11,6 ),(12,6 ),(13,6 ),
-# (5 ,7 ),(6 ,7 ),(7 ,7 ),(8 ,7 ),(9 ,7 ),(10,7 ),(11,7 ),(12,7 ),(13,7 ),
-# (6 ,8 ),(7 ,8 ),(8 ,8 ),(9 ,8 ),(10,8 ),
-# (7 ,9 ),(8 ,9 ),(9 ,9 ),(10,9 ),(11,9 ),
-# (8 ,10),(9 ,10),(10,10),(11,10),(12,10),
-# (9 ,11),(10,11),(11,11),(12,11),(13,11),
-# (10,12),(11,12),(12,12),(13,12),
-# (11,13),(12,13),(13,13),
-# (12,14),(13,14),
-# (10,15),(11,15),(12,15),(13,15),
-# (8 ,16),(9 ,16),(10,16),(11,16),(12,16),(13,16),
-# (7 ,17),(8 ,17),(9 ,17),(10,17),(11,17),(12,17),(13,17),
-# (7 ,18),(8 ,18),(9 ,18),(10,18),(11,18),(12,18),(13,18),
-# (7 ,19),(8 ,19),(9 ,19),(10,19),(11,19),(12,19),(13,19),
-# (7 ,20),(8 ,20),(9 ,20),(10,20),(11,20),(12,20),(13,20),
-# (7 ,21),(8 ,21),(9 ,21),(10,21),(11,21),(12,21),(13,21),
-# (7 ,22),(8 ,22),(9 ,22),(10,22),(11,22),(12,22),(13,22),
-# (8 ,23),(9 ,23),(10,23),(11,23),(12,23),(13,23),
-# (10,24),(11,24),(12,24),(13,24),
-# (10,25),(11,25),(12,25),(13,25),
-# (10,26),(11,26),(12,26),(13,26),
-# (10,27),(11,27),(12,27),(13,27),
-# (10,28),(11,28),(12,28),(13,28),
-# (8 ,29),(9 ,29),(10,29),(11,29),(12,29),(13,29),
-# (6 ,30),(7 ,30),(8 ,30),(9 ,30),(10,30),(11,30),(12,30),(13,30),
-# (5 ,31),(6 ,31),(7 ,31),(8 ,31),(9 ,31),(10,31),(11,31),(12,31),(13,31),
-# (5 ,32),(6 ,32),(7 ,32),(8 ,32),(9 ,32),(10,32),(11,32),(12,32),(13,32),
-# (5 ,33),(6 ,33),(7 ,33),(8 ,33),(9 ,33),(10,33),(11,33),(12,33),(13,33),
-# (5 ,34),(6 ,34),(7 ,34),(8 ,34),(9 ,34),(10,34),(11,34),(12,34),(13,34),
-# (5 ,35),(6 ,35),(7 ,35),(8 ,35),(9 ,35),(10,35),(11,35),(12,35),(13,35),
-# (6 ,36),(7 ,36),(8 ,36),(9 ,36),(10,36),(11,36),(12,36),(13,36),
-# (8 ,37),(9 ,37),(10,37),(11,37),(12,37),(13,37)
+                        .byte 0  3  1  3  2  3  3  3  4  3  5  3
+                        .byte 0  2  1  2  2  2  3  2  4  2
+                        .byte 0  1  1  1  2  1  3  1  4  1
+                        .byte 0  0  1  0  2  0  3  0  4  0
+                        .byte -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 
+                        # prevent possible overshooting
 #### Maps
 
 .text
@@ -218,8 +180,8 @@ main:
 # Fill in your code here
         li      $s0, 0                  # number of Puzzlecoins
     # Checking nearby kernels should not be performed more than once in 16000 cycles
-        lw      $s1, TIMER($zero)       # last check time
-        # s2
+        lw      $s1, TIMER($zero)       # projected check time
+        li      $s2, 8                  # max search radius
         li      $s3, 1                  # searching radius
         la      $t0, original_map
         sw      $t0, GET_MAP($zero)
@@ -274,8 +236,8 @@ should_solve:
         la      $t0, minibot_info
         sw      $t0, GET_MINIBOT_INFO($zero)
         lw      $t1, 0($t0)     # number of minibots currently have
-# Are there totoal 5 minibots?
-        blt     $t1, 5, minibot_not_enough
+# Are there totoal 3 minibots?
+        blt     $t1, 3, minibot_not_enough
         la      $t2, solve_flag
         sb      $zero, 0($t2)
 minibot_not_enough:
@@ -324,6 +286,8 @@ no_specified_destination:
         sb      $t0, 1($t1)
         j       idling
 not_enough_delay_since_bump:
+        lw      $t0, TIMER($zero)
+        ble     $t0, $s1, others
 # Checking Nearby Kernels
         la      $t9, kernel_locations   # &kernel_locations
         sw      $t9, GET_KERNEL_LOCATIONS($zero)
@@ -467,8 +431,10 @@ lft_end_for:
 # No kernels detected within radius, increase radius of search
         add     $s3, $s3, 1
 # Searching radius greater than 8, reduced to 1
-        blt     $s3, 8, radius_acceptable
+        blt     $s3, $s2, radius_acceptable
         li      $s3, 1
+        lw      $t0, TIMER($zero)
+        add     $s1, $t0, 100000
 radius_acceptable:
         j       others
 # Kernel Detected, moving to destination...
@@ -528,29 +494,39 @@ others:
         lw      $t2, 0($t2)
         lbu     $t2, 0($t2)
         bne     $t2, 2, silo_unable_to_be_built
+        li      $s2, 4
         la      $t0, silo_status
         sb      $sp, 0($t0)
 silo_already_built:
         la      $t0, current_checking_idx
         lw      $t0, 0($t0)
+        #---------------- $t0 current - current checking index
         mul     $t1, $t0, 2
         la      $t2, left_map_coordinate
-        add     $t1, $t1, $t2   # current checking pointer
+        add     $t1, $t1, $t2   
+        #---------------- $t1 current - left-map current checking tile pointer
         la      $t2, minibot_info
         sw      $t2, GET_MINIBOT_INFO($zero)
-        lw      $t3, 0($t2)     # number of bots potentially waiting
-#         bge     $t3, 3, no_need_for_suppliment
-#     # has less than 3 bots, switch the solve_flag to true
-#         la      $t4, solve_flag
-#         li      $t5, 1
-#         sb      $t5, 0($t4)
-# no_need_for_suppliment:
-        add     $t2, $t2, 4     # pointer to the first minibot struct
-        la      $t4, bot_estimate_move_time     # pointer to the first minibot estimated moving time
+        lw      $t3, 0($t2)   
+        #---------------- $t3 fixed - number of bots potentially waiting
+        add     $t2, $t2, 4
+        #---------------- $t2 fixed - pointer to the first minibot struct
+        bge     $t3, 5, no_need_for_suppliment
+    # has less than 5 bots, switch the solve_flag to true
+        la      $t4, solve_flag
+        li      $t5, 1
+        sb      $t5, 0($t4)
+        beq     $t3, 0, checking_end
+no_need_for_suppliment:
+        la      $t4, minibots_estimate_arriving_time 
+        #---------------- $t4 fixed - pointer to the first minibot estimated arriving time
         la      $t5, kernel_locations           # kernel map
         sw      $t5, GET_KERNEL_LOCATIONS($zero)
         add     $t5, $t5, 4
+        #---------------- $t5 fixed - kernel location
 bot_checking_start: 
+        la      $t6, kernel_locations
+        sw      $t6, GET_KERNEL_LOCATIONS($zero)
         lb      $a1, 0($t1)     # current checking X
         blt     $a1, $zero, checking_restart
         lb      $a2, 1($t1)     # current checking Y
@@ -561,24 +537,36 @@ bot_checking_start:
         beq     $t6, $zero, check_next_tile
     # has kernel on current tile, loop through bots to find a suitable bot
         li      $a0, 0
-not_searching_loop:
+bot_searching_loop:
         bge     $a0, $t3, no_bot_availble
-        mul     $t7, $t0, 4
+        mul     $t7, $a0, 4
         add     $t7, $t7, $t4
         lw      $t7, 0($t7)
         lw      $t8, TIMER($zero)
-        sub     $t7, $t8, $t7   # current time - estimated moving time
-        blt     $t7, $zero, time_not_enough 
+        blt     $t8, $t7, time_not_enough   
+    # current time >= estimated arriving time
+        mul     $t9, $a0, 8
+        add     $t9, $t9, $t2
+        lbu     $v0, 7($t9)
+        blt     $v0, 7, bot_kernel_not_enough
+        move    $t8, $a1
+        move    $t9, $a2
+        la      $a2, silo_position
+        lb      $a1, 0($a2)
+        lb      $a2, 1($a2)
         jal     minibot_move_to_destination
-        add     $t0, $t0, 1
-        add     $t1, $t1, 2
-        j       bot_checking_start
+        move    $a1, $t8
+        move    $a2, $t9
+        j       time_not_enough
+bot_kernel_not_enough:
+        jal     minibot_move_to_destination
+        j       check_next_tile
 time_not_enough:
+    # current time < estimated arriving time
         add     $a0, $a0, 1
-        j       not_searching_loop
+        j       bot_searching_loop
 no_bot_availble:
         la      $a3, current_checking_idx
-        add     $t0, $t0, 1
         sw      $t0, 0($a3)
         j       checking_end
 check_next_tile:
@@ -589,6 +577,16 @@ checking_restart:
         la      $t0, current_checking_idx
         sw      $zero, 0($t0)
 checking_end:
+# Check if the silo still exists
+        la      $t2, silo_position_on_map
+        lw      $t2, 0($t2)
+        lbu     $t2, 0($t2)
+        beq     $t2, 2, silo_exists
+    # silo get destroyed
+        la      $t0, silo_status
+        li      $t1, 1
+        lb      $t1, 0($t0)
+silo_exists:
 silo_unable_to_be_built:
 
         j       infinite        # Don't remove this! If this is removed, then your code will not be graded!!!
@@ -599,11 +597,10 @@ silo_unable_to_be_built:
 # $a0 - the index of the minibot (0-indexed)
 # $a1 - destination X
 # $a2 - destination Y
-# time delay = (delta_x + delta_y) * 10000
+# time delay = (delta_x + delta_y) * 10100
 minibot_move_to_destination:
         la      $a3, minibot_info
         sw      $a3, GET_MINIBOT_INFO($zero)
-        lw      $v0, 0($a3)     # ONLY FOR CHECKING
         add     $a3, $a3, 4     # pointer to the first minibot struct
         mul     $v0, $a0, 8     # offset in minibot array
         add     $a3, $a3, $v0   # pointer to current minibot struct
@@ -618,18 +615,18 @@ first_argument_vaild:
         sub     $v1, $zero, $v1
 second_argument_vaild:
         add     $v1, $v1, $v0   # delta_x + delta_y
-        mul     $v1, $v1, 10000
+        mul     $v1, $v1, 10100
         lw      $a3, 0($a3)     # ID of the minibot
         sw      $a3, SELECT_MINIBOT_BY_ID($zero)
-        sll     $a2, $a2, 8
-        or      $a2, $a2, $a1
-        sw      $a2, SET_TARGET_TILE($zero)
-        mul     $a2, $a0, 4
-        la      $a3, bot_estimate_move_time
-        add     $a3, $a3, $a2
-        lw      $a2, TIMER($zero)
-        add     $a2, $a2, $v1
-        sw      $a2, 0($a3)
+        sll     $v0, $a2, 8
+        or      $v0, $v0, $a1
+        sw      $v0, SET_TARGET_TILE($zero)
+        mul     $v0, $a0, 4
+        la      $a3, minibots_estimate_arriving_time
+        add     $a3, $a3, $v0
+        lw      $v0, TIMER($zero)
+        add     $v0, $v0, $v1
+        sw      $v0, 0($a3)
         jr      $ra
 # minibot_move_to_destination END
 #------------------------------------------------------------------------------
@@ -654,7 +651,7 @@ move_to_destination:
         sw      $v0, ANGLE_CONTROL($zero)
         jal     euclidean_dist
         lw      $t0, TIMER($zero)       # get current time
-        add     $t0, $v0, $t0           # estimated moving time
+        add     $t0, $v0, $t0           # estimated arriving time
         sw      $t0, TIMER($zero)       # set timer interrupt
         li      $t0, 10
         sw      $t0, VELOCITY($zero)    # full speed
